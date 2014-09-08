1f4340f289421dec46dbf0b52736b1681af6519b
Solomon Hykes <solomon@docker.com>
2014-09-08 03:05:46 +0000

    Read commands from stdin
    Signed-off-by: Solomon Hykes <solomon@docker.com>

--- setns.go
+++ setns.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"flag"
+	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"strings"
@@ -12,36 +13,96 @@ import (
 )
 
 func main() {
-	netnsPath := flag.String("net", "", "Path to the NET namespace to enter")
-	flag.Parse()
-	if flag.NArg() == 0 {
-		fmt.Fprintf(os.Stderr, "Usage: %s [OPTIONS] PATH [ARGS...]\n", os.Args[0])
+	if err := serveCommands(os.Stdin); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
-	if *netnsPath != "" {
-		netns, err := os.Open(*netnsPath)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "exec: %v\n", err)
-			os.Exit(1)
+}
+
+func serveCommands(input io.Reader) error {
+	cmds := json.NewDecoder(input)
+	for {
+		var cmd []string
+		if err := cmds.Decode(&cmd); err == io.EOF {
+			return nil
+		} else if err != nil {
+			return fmt.Errorf("json decode: %v", err)
 		}
-		if err := system.Setns(netns.Fd(), syscall.CLONE_NEWNET); err != nil {
-			fmt.Fprintf(os.Stderr, "setns: %v\n", err)
-			os.Exit(1)
+		if len(cmd) == 0 {
+			fmt.Fprintf(os.Stderr, "skipping empty command\n", cmd)
+			continue
+		}
+		if err := do(cmd[0], cmd[1:]...); err != nil {
+			return err
 		}
 	}
-	var (
-		cmdName = flag.Args()[0]
-	)
+}
+
+func do(name string, args ...string) error {
+	switch name {
+	case "netns":
+		{
+			return doNetns(args...)
+		}
+	case "mntns":
+		{
+			return doMntns(args...)
+		}
+	case "exec":
+		{
+			return doExec(args...)
+		}
+	case "setenv":
+		{
+			return doSetenv(args...)
+		}
+	}
+	return fmt.Errorf("no such command: %s", name)
+}
+
+func doSetenv(args ...string) error {
+	if len(args) != 2 {
+		return fmt.Errorf("usage: setenv KEY VALUE")
+	}
+	os.Setenv(args[0], args[1])
+	return nil
+}
+
+func doExec(args ...string) error {
+	if len(args) < 1 {
+		return fmt.Errorf("usage: exec NAME [ARGS...]")
+	}
+	var cmdName = args[0]
 	if !strings.Contains(cmdName, "/") {
 		var err error
 		cmdName, err = exec.LookPath(cmdName)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "%v\n", err)
-			os.Exit(1)
+			return err
 		}
 	}
-	if err := syscall.Exec(cmdName, flag.Args(), os.Environ()); err != nil {
-		fmt.Fprintf(os.Stderr, "exec: %v\n", err)
-		os.Exit(1)
+	return syscall.Exec(cmdName, args, os.Environ())
+}
+
+func doNetns(args ...string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("usage: netns PATH")
 	}
+	var nsPath = args[0]
+	ns, err := os.Open(nsPath)
+	if err != nil {
+		return err
+	}
+	return system.Setns(ns.Fd(), syscall.CLONE_NEWNET)
+}
+
+func doMntns(args ...string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("usage: mntns PATH")
+	}
+	var nsPath = args[0]
+	ns, err := os.Open(nsPath)
+	if err != nil {
+		return err
+	}
+	return system.Setns(ns.Fd(), syscall.CLONE_NEWNS)
 }
